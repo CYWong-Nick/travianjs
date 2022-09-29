e49dd75f1da7edaf514dddfc0604c4411c056bec
Desmond Lai <desmond.lai.ibm@gmail.com>
2022-09-29 22:10:00 +0800

    Deploy

--- dist/travian_v2.js
+++ dist/travian_v2.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var _a, _b;
-const BUILD_TIME = "2022/09/29 21:56:37";
+const BUILD_TIME = "2022/09/29 22:10:00";
 const RUN_INTERVAL = 10000;
 const GID_NAME_MAP = {
     "1": "Woodcutter",
