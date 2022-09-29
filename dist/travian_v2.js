61de4c8a8435c2257a48313a5f999bbe57f7dda2
Nick Wong <nick.wong1@ibm.com>
2022-09-29 22:28:35 +0800

    Deploy

--- dist/travian_v2.js
+++ dist/travian_v2.js
@@ -9,7 +9,7 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var _a, _b;
-const BUILD_TIME = "2022/09/29 22:27:09";
+const BUILD_TIME = "2022/09/29 22:28:35";
 const RUN_INTERVAL = 10000;
 const GID_NAME_MAP = {
     "1": "Woodcutter",
